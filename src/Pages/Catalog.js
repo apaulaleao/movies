b3238a313380e1fcae5779ecbde482dbd2a9a57c
apaulaleao <paula.leao@gmail.com>
2022-02-04 22:18:15 -0300

    load

--- src/Pages/Catalog.js
+++ src/Pages/Catalog.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 
 import PageHeader from '../components/PageHeader/PageHeader';
 import { category as cate} from '../Api/tmdbApi';
+import MovieGrid from '../components/MovieGrid/MovieGrid';
 
 const Catalog = () => {
 
@@ -12,6 +13,13 @@ const Catalog = () => {
     <PageHeader>
       {category === cate.movie ? 'Movies' : 'TV Series'}
       </PageHeader>
+     <div className='container'>
+       <div className='section mb-3'>
+         <MovieGrid category ={category} />
+       </div>
+     </div>
+
+
 </>
   ) 
 };
